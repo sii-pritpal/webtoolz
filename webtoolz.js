1a0fe0c44dedf86aa7c9c9f7577c08eb60aa8482
sii-pritpal <sii.pritpal@gmail.com>
2014-01-31 00:56:55 +0530

    Update webtoolz.js

--- webtoolz.js
+++ webtoolz.js
@@ -1,1 +1,4 @@
 document.writeln('<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js" charset="utf-8"></script>');
+$(document).ready(function(){
+  alert("works...");
+});
